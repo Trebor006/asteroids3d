7247bea9c032cb9a134662f65ebbc415c28c0138
Robert Cabrera <clrobert.06@gmail.com>
2023-07-15 02:46:16 -0400

    Fondo

--- core/src/com/mibu/asteroids3d/screen/GameScreen.java
+++ core/src/com/mibu/asteroids3d/screen/GameScreen.java
@@ -2,14 +2,21 @@ package com.mibu.asteroids3d.screen;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.mibu.asteroids3d.controller.KeyController;
 import com.mibu.asteroids3d.objects.Spaceship;
 import com.mibu.asteroids3d.objects.Stage;
+import com.mibu.asteroids3d.util.AssetUtils;
 import com.mibu.asteroids3d.util.CameraUtil;
 
 public class GameScreen extends BaseScreen {
     private Stage stage;
     private KeyController inputController;
+    private SpriteBatch batch;
+    private Texture texture;
+    private TextureRegion region;
 
     public GameScreen() {
         this.stage = new Stage();
@@ -21,6 +28,9 @@ public class GameScreen extends BaseScreen {
         inputController.setSpaceship(naveActor);
 
         Gdx.input.setInputProcessor(inputController);
+        batch = new SpriteBatch();
+        texture = new Texture(Gdx.files.internal(AssetUtils.fondo));
+        region = new TextureRegion(texture, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
     }
 
     @Override
@@ -32,6 +42,11 @@ public class GameScreen extends BaseScreen {
         CameraUtil.getCamera().update();
         Gdx.gl.glClearColor(0.1f, 0.1f, 0.1f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+
+        batch.begin();
+        batch.draw(region, 0, 0);
+        batch.end();
+
         stage.act();
         stage.draw();
     }
---
+++ core/src/com/mibu/asteroids3d/util/AssetUtils.java
@@ -0,0 +1,6 @@
+package com.mibu.asteroids3d.util;
+
+public class AssetUtils {
+
+    public static String fondo = "assets/fondos/space_1_360.png";
+}
