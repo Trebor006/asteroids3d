31e187ef12bd97e6e0db23caac61aa498ee6f02d
Robert Cabrera <clrobert.06@gmail.com>
2023-07-15 03:03:54 -0400

    add sonido disparo

--- core/src/com/mibu/asteroids3d/util/AssetUtils.java
+++ core/src/com/mibu/asteroids3d/util/AssetUtils.java
@@ -3,4 +3,5 @@ package com.mibu.asteroids3d.util;
 public class AssetUtils {
 
     public static String fondo = "assets/fondos/space_1_360.png";
+    public static String disparo = "assets/sound/shot.mp3";
 }
